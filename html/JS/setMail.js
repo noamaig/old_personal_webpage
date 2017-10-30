1f29820370b1e5aaa82a49ea2d66d192cf9d1dce
Noam Aigerman <noamaig@users.noreply.github.com>
2017-10-30 18:16:47 +0200

    Update setMail.js

--- html/JS/setMail.js
+++ html/JS/setMail.js
@@ -1,5 +1,5 @@
 function setMail() {
-    a1 = "nssossasmss.sasisgssesrssmsasnss@sssissnssfsss.estshsszss.ssschsss";
+    a1 = "nssssssssssosssassssssssssssmssssasssssssssssssssisssssgssesrssmssassnssssssss@ssssgsssmssssasssssissslssss.sssscsssosssssmsss";
     address = a1.replace(/s/g,"");
     e = "<a href=\"mailto:" + address + "\" target=\"_blank\" >" + address + "</a> ";
     $(".emailplace").html(e);
